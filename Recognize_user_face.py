33e93a702744511b17fc2a20b2ce7e25d22a09a8
Subha Chakraborty <86897887+substobeme@users.noreply.github.com>
2024-09-08 09:05:56 +0530

    Update Recognize_user_face.py

--- Recognize_user_face.py
+++ Recognize_user_face.py
@@ -1,16 +1,15 @@
 import cv2
 import mysql.connector
-from datetime import datetime
+from datetime import datetime, timedelta
 
-# Initialize MySQL connection
+
 db_connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="*********", # my password
+    password="********",
     database="face_info"
 )
 cursor = db_connection.cursor()
-
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -18,6 +17,13 @@ face_recognizer.read('face_recognizer.ymnl')
 
 width, height = 220, 220
 camera = cv2.VideoCapture(0)
+
+
+last_recognized = {
+    "Subha": datetime.now() - timedelta(seconds=2),
+    "Ashutosh": datetime.now() - timedelta(seconds=2),
+    "Akash": datetime.now() - timedelta(seconds=2)
+}
 
 while True:
     check, frame = camera.read()
@@ -30,21 +36,24 @@ while True:
         pred, conf = face_recognizer.predict(frame_face)
         identity = "unknown"
 
-        if pred == 1 and conf > 0.5:
+        if pred == 1 and conf > 0.95:
             identity = "Subha"
-        elif pred == 2 and conf > 0.5:
+        elif pred == 2 and conf > 0.95:
             identity = "Ashutosh"
-        elif pred == 3 and conf > 0.5:
+        elif pred == 3 and conf > 0.95:
             identity = "Akash"
 
         cv2.putText(frame, identity, (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 1)
 
-
         if identity != "unknown":
-            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            insert_query = "INSERT INTO FaceRecord (Name,Time) VALUES (%s, %s)"
-            cursor.execute(insert_query, (identity, current_time))
-            db_connection.commit()
+            current_time = datetime.now()
+            if current_time - last_recognized[identity] >= timedelta(seconds=1):
+
+                insert_query = "INSERT INTO face(Name, Time, Photo) VALUES (%s, %s, %s)"
+                image_bytes = cv2.imencode('.jpg', frame_face)[1].tobytes()
+                cursor.execute(insert_query, (identity, current_time, image_bytes))
+                db_connection.commit()
+                last_recognized[identity] = current_time
 
     cv2.imshow("Recognizer", frame)
     if cv2.waitKey(1) == ord('q'):
